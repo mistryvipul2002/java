6a49c6c3b465ead1c05eb15e6ada311149a982fb
Vipul Mistry <vipul_mistry@intuit.com>
2020-12-05 22:57:48 -0800

    Removed Binary tree

--- src/main/java/com/programming/exercises/practice/tree/BalancedBinaryTree.java
+++ src/main/java/com/programming/exercises/practice/tree/BalancedBinaryTree.java
@@ -1,16 +1,16 @@
 package com.programming.exercises.practice.tree;
 
-import com.programming.exercises.practice.datastructure.BinaryTree;
+import com.programming.exercises.practice.datastructure.TreeNode;
 import org.apache.commons.lang3.RandomUtils;
 
 public class BalancedBinaryTree {
 
     public static void main(String[] args) {
-        BinaryTree<Integer> binaryTree = new BinaryTree<Integer>().root(RandomUtils.nextInt() % 100);
+        TreeNode<Integer> treeNode = new TreeNode<Integer>(RandomUtils.nextInt() % 100);
         for (int i = 0; i < 10; i++) {
-            binaryTree.root.addBalanced(RandomUtils.nextInt() % 100);
+            treeNode.addBalanced(RandomUtils.nextInt() % 100);
         }
 
-        System.out.println(binaryTree);
+        System.out.println(treeNode);
     }
 }
--- src/main/java/com/programming/exercises/practice/tree/RandomBinaryTree.java
+++ src/main/java/com/programming/exercises/practice/tree/RandomBinaryTree.java
@@ -1,14 +1,14 @@
 package com.programming.exercises.practice.tree;
 
-import com.programming.exercises.practice.datastructure.BinaryTree;
+import com.programming.exercises.practice.datastructure.TreeNode;
 import org.apache.commons.lang3.RandomUtils;
 
 public class RandomBinaryTree {
 
     public static void main(String[] args) {
-        BinaryTree<Integer> integerBinaryTree = new BinaryTree<Integer>().root(RandomUtils.nextInt() % 100);
-        integerBinaryTree.root.populateRandom(integerBinaryTree.root, 5);
+        TreeNode<Integer> treeNode = new TreeNode<Integer>(RandomUtils.nextInt() % 100);
+        treeNode.populateRandom(treeNode, 5);
 
-        System.out.println(integerBinaryTree);
+        System.out.println(treeNode);
     }
 }
