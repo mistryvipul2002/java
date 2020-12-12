08abea964c78be635a7cfc5a6aedcaa1a049112e
Vipul Mistry <vipul_mistry@intuit.com>
2020-12-11 18:36:13 -0800

    bfs

--- src/main/java/com/programming/exercises/practice/datastructure/Graph.java
+++ src/main/java/com/programming/exercises/practice/datastructure/Graph.java
@@ -7,30 +7,16 @@ import java.util.Set;
 
 public class Graph<T extends Comparable<T>, W extends Comparable<W>> {
     private Map<T, Vertex<T, W>> vertexMap = new HashMap<>();
-    private Map<Vertex<T, W>, Boolean> visited = new HashMap<>();
+    public Set<Vertex<T, W>> visited = new HashSet<>();
     private Map<Vertex<T, W>, Set<Vertex<T, W>>> graphMap = new HashMap<>();
     public boolean isDirected = false;
 
-    public Vertex<T, W> getAnyVertex() {
-        return vertexMap.entrySet().stream().findAny().get().getValue();
+    public Graph(boolean isDirected) {
+        this.isDirected = isDirected;
     }
 
-    public void edge(T source, T dest) {
-        if (source.compareTo(dest) == 0) return;
-
-        final Vertex<T, W> sourceVertex = getVertex(source);
-        if (!graphMap.containsKey(sourceVertex)) {
-            graphMap.put(sourceVertex, new HashSet<>());
-        }
-        graphMap.get(sourceVertex).add(getVertex(dest));
-
-        if (!isDirected) {
-            final Vertex<T, W> destVertex = getVertex(dest);
-            if (!graphMap.containsKey(destVertex)) {
-                graphMap.put(destVertex, new HashSet<>());
-            }
-            graphMap.get(destVertex).add(getVertex(source));
-        }
+    public Vertex<T, W> getAnyVertex() {
+        return vertexMap.entrySet().stream().findAny().get().getValue();
     }
 
     private Vertex<T, W> getVertex(T data) {
@@ -38,6 +24,24 @@ public class Graph<T extends Comparable<T>, W extends Comparable<W>> {
             vertexMap.put(data, new Vertex<>(data));
         }
         return vertexMap.get(data);
+    }
+
+    public void addEdge(T source, T dest) {
+        if (source.compareTo(dest) == 0) return;
+        final Vertex<T, W> sourceVertex = getVertex(source);
+        final Vertex<T, W> destVertex = getVertex(dest);
+
+        if (!graphMap.containsKey(sourceVertex)) {
+            graphMap.put(sourceVertex, new HashSet<>());
+        }
+        graphMap.get(sourceVertex).add(destVertex);
+
+        if (!isDirected) {
+            if (!graphMap.containsKey(destVertex)) {
+                graphMap.put(destVertex, new HashSet<>());
+            }
+            graphMap.get(destVertex).add(sourceVertex);
+        }
     }
 
     @Override
@@ -58,15 +62,15 @@ public class Graph<T extends Comparable<T>, W extends Comparable<W>> {
 
         while (!stack.isEmpty()) {
             final Vertex<T, W> popped = stack.pop();
-            if (visited.containsKey(popped)) continue;
+            if (visited.contains(popped)) continue;
 
-            visited.put(popped, true);
+            visited.add(popped);
             sb.append(popped.data + ",");
 
             if (graphMap.containsKey(popped)) {
                 // replace it with connected nodes
                 for (Vertex<T, W> connectedVertex : graphMap.get(popped)) {
-                    if (visited.containsKey(connectedVertex)) continue;
+                    if (visited.contains(connectedVertex)) continue;
                     stack.push(connectedVertex);
                 }
             }
@@ -78,17 +82,45 @@ public class Graph<T extends Comparable<T>, W extends Comparable<W>> {
 
     public void dfsWithRecursion(T data, StringBuffer sb) {
         final Vertex<T, W> dataVertex = getVertex(data);
-        if (dataVertex == null || visited.containsKey(dataVertex)) return;
+        visited.add(dataVertex);
+        sb.append(data + ",");
 
         if (graphMap.containsKey(dataVertex)) {
             for (Vertex<T, W> connectedVertex : graphMap.get(dataVertex)) {
-                if (!visited.containsKey(connectedVertex)) {
-                    visited.put(dataVertex, true);
-                    sb.append(connectedVertex.data + ",");
+                if (!visited.contains(connectedVertex)) {
                     dfsWithRecursion(connectedVertex.data, sb);
                 }
             }
         }
+    }
+
+    public String bfsWithQueue(T data) {
+        StringBuffer sb = new StringBuffer();
+        final Vertex<T, W> dataVertex = getVertex(data);
+        if (dataVertex == null) return null;
+
+        Queue<Vertex<T, W>> queue = new Queue<>();
+        queue.enqueue(dataVertex);
+        visited.add(dataVertex);
+        sb.append(dataVertex.data + ",");
+
+        while (!queue.isEmpty()) {
+            final Vertex<T, W> popped = queue.dequeue().data;
+
+            if (graphMap.containsKey(popped)) {
+                // replace it with connected nodes
+                for (Vertex<T, W> connectedVertex : graphMap.get(popped)) {
+                    if (visited.contains(connectedVertex)) continue;
+                    
+                    queue.enqueue(connectedVertex);
+                    visited.add(connectedVertex);
+                    sb.append(connectedVertex.data + ",");
+                }
+            }
+        }
+
+        visited.clear();
+        return sb.toString();
     }
 
 }
--- src/main/java/com/programming/exercises/practice/graph/GraphUtil.java
+++ src/main/java/com/programming/exercises/practice/graph/GraphUtil.java
@@ -1,32 +1,37 @@
 package com.programming.exercises.practice.graph;
 
 import com.programming.exercises.practice.datastructure.Graph;
-import com.programming.exercises.practice.datastructure.Vertex;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class GraphUtil {
 
-    public static Graph<String, Integer> randomDirectedGraph() {
-        Graph<String, Integer> graph = new Graph<>();
-        graph.isDirected = true;
+    public static Graph<String, Integer> randomGraph(boolean isDirected) {
+        Graph<String, Integer> graph = new Graph<>(isDirected);
         for (int i = 0; i < 10; i++) {
-            graph.edge(RandomStringUtils.randomNumeric(1), RandomStringUtils.randomNumeric(1));
+            graph.addEdge(RandomStringUtils.randomNumeric(1), RandomStringUtils.randomNumeric(1));
         }
-
         return graph;
     }
 
     public static void main(String[] args) {
-        final Graph<String, Integer> graph = randomDirectedGraph();
+        final Graph<String, Integer> graph = randomGraph(true);
         System.out.println(graph);
 
+        final String dataStartingNode = graph.getAnyVertex().data;
+        System.out.println("Starting node -> " + dataStartingNode);
+        System.out.println();
+
         // dfs with stack
-        final Vertex<String, Integer> anyVertex = graph.getAnyVertex();
-        System.out.println(graph.dfsWithStack(anyVertex.data));
+        System.out.println("DFS with stack -> " + graph.dfsWithStack(dataStartingNode));
+        graph.visited.clear();
 
         // dfs with recursion
-        StringBuffer sb = new StringBuffer(anyVertex.data + ",");
-        graph.dfsWithRecursion(anyVertex.data, sb);
-        System.out.println(sb.toString());
+        StringBuffer sb = new StringBuffer();
+        graph.dfsWithRecursion(dataStartingNode, sb);
+        System.out.println("DFS with recursion -> " + sb.toString());
+        graph.visited.clear();
+
+        // bfs with queue
+        System.out.println("BFS with queue -> " + graph.bfsWithQueue(dataStartingNode));
     }
 }
